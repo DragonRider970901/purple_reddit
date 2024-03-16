2761b971d501a1210abed8a1d3309fe468a13109
Inana Susa <inana.programming@gmail.com>
2024-03-16 10:34:16 +0200

    Created layout files for routing

---
+++ src/layout/CategoriesLayout.js
@@ -0,0 +1,9 @@
+import React from "react"; 
+
+export default function CategoriesLayout() {
+    return (
+        <div>
+
+        </div>
+    );
+};
---
+++ src/layout/ProfileLayout.js
@@ -0,0 +1,9 @@
+import React from "react"; 
+
+export default function ProfileLayout() {
+    return (
+        <div>
+
+        </div>
+    );
+};
